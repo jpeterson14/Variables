4fe7c8b7ad95c5c31e330a41a155d925bdce5c18
jpeterson0514 <>
2021-05-06 18:43:20 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+'This is data, it is a string. Strings start with " "'
+
+"Part of being data, or a string, is that ruby doesn't interpret it."
+
+puts 1+1
+puts "1+1"
+
+example = "The word 'example' is equal to this sentence, it's a named variable."
+
+puts example
+
+name = "Jake"
+age = 23
+
+intro = "Hi my name is #{name}. I am #{age} years old."
+puts intro
